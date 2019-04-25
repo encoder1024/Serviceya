ac46c24d76eff3dc76826017e3a01ebbd4cc85f4
encoder1024 <48453885+encoder1024@users.noreply.github.com>
2019-04-25 12:42:43 -0300

    Pruebas de las animaciones para el menú principal...

--- app/src/main/java/com/domo/zoom/serviceya/MainActivity.java
+++ app/src/main/java/com/domo/zoom/serviceya/MainActivity.java
@@ -1,5 +1,8 @@
 package com.domo.zoom.serviceya;
 
+import android.animation.Animator;
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,9 +15,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewAnimationUtils;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private LinearLayout fabContainer;
+    private FloatingActionButton fab, fab1, fab2, fab3;
+    private boolean fabMenuOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +35,21 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fabContainer = (LinearLayout) findViewById(R.id.fabContainerLayout);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                toggleFabMenu();
             }
         });
+
+        fab1 = (FloatingActionButton) findViewById(R.id.fab_1);
+        fab2 = (FloatingActionButton) findViewById(R.id.fab_2);
+        fab3 = (FloatingActionButton) findViewById(R.id.fab_3);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,7 +59,115 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
     }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void toggleFabMenu() {
+
+        //Animations
+        Animation show_fab_1 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab1_show);
+        Animation hide_fab_1 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab1_hide);
+        Animation show_fab_2 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab2_show);
+        Animation hide_fab_2 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab2_hide);
+        Animation show_fab_3 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab3_show);
+        Animation hide_fab_3 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab3_hide);
+
+        if (!fabMenuOpen) {
+            fabContainer.setVisibility(View.VISIBLE);
+            fab.setImageResource(R.drawable.ic_close);
+            int centerX = fabContainer.getWidth() / 2;
+            int centerY = fabContainer.getHeight() / 2;
+            int startRadius = 0;
+            int endRadius = (int) Math.hypot(fabContainer.getWidth(), fabContainer.getHeight()) / 2;
+
+
+            ViewAnimationUtils
+                    .createCircularReveal(
+                            fabContainer,
+                            centerX,
+                            centerY,
+                            startRadius,
+                            endRadius
+                    )
+                    .setDuration(1000)
+                    .start();
+            FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) fab1.getLayoutParams();
+            layoutParams.rightMargin += (int) (fab1.getWidth() * 1.7);
+            layoutParams.bottomMargin += (int) (fab1.getHeight() * 0.25);
+            fab1.setLayoutParams(layoutParams);
+            fab1.startAnimation(show_fab_1);
+            fab1.setClickable(true);
+            layoutParams = (FrameLayout.LayoutParams) fab2.getLayoutParams();
+            layoutParams.rightMargin += (int) (fab2.getWidth() * 1.5);
+            layoutParams.bottomMargin += (int) (fab2.getHeight() * 1.5);
+            fab2.setLayoutParams(layoutParams);
+            fab2.startAnimation(show_fab_2);
+            fab2.setClickable(true);
+            layoutParams = (FrameLayout.LayoutParams) fab3.getLayoutParams();
+            layoutParams.rightMargin += (int) (fab3.getWidth() * 0.25);
+            layoutParams.bottomMargin += (int) (fab3.getHeight() * 1.7);
+            fab3.setLayoutParams(layoutParams);
+            fab3.startAnimation(show_fab_3);
+            fab3.setClickable(true);
+        } else {
+            fab.setImageResource(R.drawable.ic_add);
+            int centerX = fabContainer.getWidth() / 2;
+            int centerY = fabContainer.getHeight() / 2;
+            int startRadius = (int) Math.hypot(fabContainer.getWidth(), fabContainer.getHeight()) / 2;
+            int endRadius = 0;
+
+            Animator animator = ViewAnimationUtils
+                    .createCircularReveal(
+                            fabContainer,
+                            centerX,
+                            centerY,
+                            startRadius,
+                            endRadius
+                    );
+            animator.setDuration(1000);
+            animator.addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animation) {
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    fabContainer.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animation) {
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animation) {
+                }
+            });
+            animator.start();
+            FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) fab1.getLayoutParams();
+            layoutParams.rightMargin -= (int) (fab1.getWidth() * 1.7);
+            layoutParams.bottomMargin -= (int) (fab1.getHeight() * 0.25);
+            fab1.setLayoutParams(layoutParams);
+            fab1.startAnimation(hide_fab_1);
+            fab1.setClickable(false);
+            layoutParams = (FrameLayout.LayoutParams) fab2.getLayoutParams();
+            layoutParams.rightMargin -= (int) (fab2.getWidth() * 1.5);
+            layoutParams.bottomMargin -= (int) (fab2.getHeight() * 1.5);
+            fab2.setLayoutParams(layoutParams);
+            fab2.startAnimation(hide_fab_2);
+            fab2.setClickable(false);
+            layoutParams = (FrameLayout.LayoutParams) fab3.getLayoutParams();
+            layoutParams.rightMargin -= (int) (fab3.getWidth() * 0.25);
+            layoutParams.bottomMargin -= (int) (fab3.getHeight() * 1.7);
+            fab3.setLayoutParams(layoutParams);
+            fab3.startAnimation(hide_fab_3);
+            fab3.setClickable(false);
+        }
+        fabMenuOpen = !fabMenuOpen;
+    }
+
 
     @Override
     public void onBackPressed() {
