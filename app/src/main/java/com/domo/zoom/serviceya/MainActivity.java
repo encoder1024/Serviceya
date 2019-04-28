7d5606975148b41bd04ef4868a32b23236706e12
encoder1024 <48453885+encoder1024@users.noreply.github.com>
2019-04-28 19:11:09 -0300

    mejoras en navegabilidad 3. Mejoras y deatalles del menú.

--- app/src/main/java/com/domo/zoom/serviceya/MainActivity.java
+++ app/src/main/java/com/domo/zoom/serviceya/MainActivity.java
@@ -20,12 +20,14 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private LinearLayout fabContainer;
     private FloatingActionButton fab, fab1, fab2, fab3, fab4, fab5, fab6, fab7, fab8;
+    private TextView tvFab1, tvFab2, tvFab3, tvFab4, tvFab5, tvFab6, tvFab7, tvFab8;
     private boolean fabMenuOpen = false;
     private boolean toolbarOpen = false;
     private Toolbar myToolbar;
@@ -59,7 +61,13 @@ public class MainActivity extends AppCompatActivity
         fab6 = findViewById(R.id.fab_6);
         fab7 = findViewById(R.id.fab_7);
         fab8 = findViewById(R.id.fab_8);
-
+        tvFab1 = findViewById(R.id.tv_fab_1);
+        tvFab2 = findViewById(R.id.tv_fab_2);
+        tvFab3 = findViewById(R.id.tv_fab_3);
+        tvFab4 = findViewById(R.id.tv_fab_4);
+        tvFab5 = findViewById(R.id.tv_fab_5);
+        tvFab6 = findViewById(R.id.tv_fab_6);
+        tvFab8 = findViewById(R.id.tv_fab_8);
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -132,6 +140,21 @@ public class MainActivity extends AppCompatActivity
         Animation show_fab_8 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab8_show);
         Animation hide_fab_8 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab8_hide);
 
+        Animation show_tvfab_1 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab1_show);
+        Animation hide_tvfab_1 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab1_hide);
+        Animation show_tvfab_2 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab2_show);
+        Animation hide_tvfab_2 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab2_hide);
+        Animation show_tvfab_3 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab3_show);
+        Animation hide_tvfab_3 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab3_hide);
+        Animation show_tvfab_4 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab4_show);
+        Animation hide_tvfab_4 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab4_hide);
+        Animation show_tvfab_5 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab5_show);
+        Animation hide_tvfab_5 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab5_hide);
+        Animation show_tvfab_6 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab6_show);
+        Animation hide_tvfab_6 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab6_hide);
+        Animation show_tvfab_8 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab8_show);
+        Animation hide_tvfab_8 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab8_hide);
+
 
         if (!fabMenuOpen) {
             int centerX = fabContainer.getWidth() / 2;
@@ -178,51 +201,105 @@ public class MainActivity extends AppCompatActivity
             fab1.setLayoutParams(layoutParams);
             fab1.startAnimation(show_fab_1);
             fab1.setClickable(true);
+            layoutParams = (FrameLayout.LayoutParams) tvFab1.getLayoutParams();
+            layoutParams.rightMargin += (int) (tvFab1.getWidth() * 3.8);
+            layoutParams.bottomMargin += (int) (tvFab1.getHeight() * 0);
+            tvFab1.setLayoutParams(layoutParams);
+            tvFab1.startAnimation(show_tvfab_1);
+            tvFab1.setClickable(true);
+
             layoutParams = (FrameLayout.LayoutParams) fab2.getLayoutParams();
             layoutParams.rightMargin += (int) (fab2.getWidth() * 1.1);
             layoutParams.bottomMargin += (int) (fab2.getHeight() * 1.1);
             fab2.setLayoutParams(layoutParams);
             fab2.startAnimation(show_fab_2);
             fab2.setClickable(true);
+            layoutParams = (FrameLayout.LayoutParams) tvFab2.getLayoutParams();
+            layoutParams.rightMargin += (int) (tvFab2.getWidth() * 2.4);
+            layoutParams.bottomMargin += (int) (tvFab2.getHeight() * 4.3);
+            tvFab2.setLayoutParams(layoutParams);
+            tvFab2.startAnimation(show_tvfab_2);
+            tvFab2.setClickable(true);
+
             layoutParams = (FrameLayout.LayoutParams) fab3.getLayoutParams();
             layoutParams.rightMargin += (int) (fab3.getWidth() * 0);
             layoutParams.bottomMargin += (int) (fab3.getHeight() * 1.3);
             fab3.setLayoutParams(layoutParams);
             fab3.startAnimation(show_fab_3);
             fab3.setClickable(true);
+            layoutParams = (FrameLayout.LayoutParams) tvFab3.getLayoutParams();
+            layoutParams.rightMargin += (int) (tvFab3.getWidth() * 0);
+            layoutParams.bottomMargin += (int) (tvFab3.getHeight() * 6.3);
+            tvFab3.setLayoutParams(layoutParams);
+            tvFab3.startAnimation(show_tvfab_3);
+            tvFab3.setClickable(true);
+
             layoutParams = (FrameLayout.LayoutParams) fab4.getLayoutParams();
             layoutParams.rightMargin -= (int) (fab4.getWidth() * 1.1);
             layoutParams.bottomMargin += (int) (fab4.getHeight() * 1.1);
             fab4.setLayoutParams(layoutParams);
             fab4.startAnimation(show_fab_4);
             fab4.setClickable(true);
+            layoutParams = (FrameLayout.LayoutParams) tvFab4.getLayoutParams();
+            layoutParams.rightMargin -= (int) (tvFab4.getWidth() * 2.5);
+            layoutParams.bottomMargin += (int) (tvFab4.getHeight() * 4.6);
+            tvFab4.setLayoutParams(layoutParams);
+            tvFab4.startAnimation(show_tvfab_4);
+            tvFab4.setClickable(true);
+
             layoutParams = (FrameLayout.LayoutParams) fab5.getLayoutParams();
             layoutParams.rightMargin -= (int) (fab5.getWidth() * 1.3);
             layoutParams.bottomMargin -= (int) (fab5.getHeight() * 0);
             fab5.setLayoutParams(layoutParams);
             fab5.startAnimation(show_fab_5);
             fab5.setClickable(true);
+            layoutParams = (FrameLayout.LayoutParams) tvFab5.getLayoutParams();
+            layoutParams.rightMargin -= (int) (tvFab5.getWidth() * 5.0);
+            layoutParams.bottomMargin -= (int) (tvFab5.getHeight() * 0);
+            tvFab5.setLayoutParams(layoutParams);
+            tvFab5.startAnimation(show_tvfab_5);
+            tvFab5.setClickable(true);
+
             layoutParams = (FrameLayout.LayoutParams) fab6.getLayoutParams();
             layoutParams.rightMargin -= (int) (fab6.getWidth() * 1.1);
             layoutParams.bottomMargin -= (int) (fab6.getHeight() * 1.1);
             fab6.setLayoutParams(layoutParams);
             fab6.startAnimation(show_fab_6);
             fab6.setClickable(true);
+            layoutParams = (FrameLayout.LayoutParams) tvFab6.getLayoutParams();
+            layoutParams.rightMargin -= (int) (tvFab6.getWidth() * 1.3);
+            layoutParams.bottomMargin -= (int) (tvFab6.getHeight() * 5.1);
+            tvFab6.setLayoutParams(layoutParams);
+            tvFab6.startAnimation(show_tvfab_6);
+            tvFab6.setClickable(true);
+
             layoutParams = (FrameLayout.LayoutParams) fab7.getLayoutParams();
             layoutParams.rightMargin -= (int) (fab7.getWidth() * 0);
             layoutParams.bottomMargin -= (int) (fab7.getHeight() * 1.3);
             fab7.setLayoutParams(layoutParams);
             fab7.startAnimation(show_fab_7);
             fab7.setClickable(true);
+
             layoutParams = (FrameLayout.LayoutParams) fab8.getLayoutParams();
             layoutParams.rightMargin += (int) (fab8.getWidth() * 1.1);
             layoutParams.bottomMargin -= (int) (fab8.getHeight() * 1.1);
             fab8.setLayoutParams(layoutParams);
             fab8.startAnimation(show_fab_8);
             fab8.setClickable(true);
+            layoutParams = (FrameLayout.LayoutParams) tvFab8.getLayoutParams();
+            layoutParams.rightMargin += (int) (tvFab8.getWidth() * 1.3);
+            layoutParams.bottomMargin -= (int) (tvFab8.getHeight() * 5.1);
+            tvFab8.setLayoutParams(layoutParams);
+            tvFab8.startAnimation(show_tvfab_8);
+            tvFab8.setClickable(true);
+
+
+
 
 
         } else {
+
+
             fab.setImageResource(R.drawable.ic_add);
             int centerX = fabContainer.getWidth() / 2;
             int centerY = fabContainer.getHeight() / 2;
@@ -242,6 +319,8 @@ public class MainActivity extends AppCompatActivity
                 @Override
                 public void onAnimationStart(Animator animation) {
                     myToolbar.setVisibility(View.GONE);
+                    tvFab1.setVisibility(View.INVISIBLE);
+                    tvFab2.setVisibility(View.INVISIBLE);
                 }
 
                 @Override
@@ -265,48 +344,97 @@ public class MainActivity extends AppCompatActivity
             fab1.setLayoutParams(layoutParams);
             fab1.startAnimation(hide_fab_1);
             fab1.setClickable(false);
+            layoutParams = (FrameLayout.LayoutParams) tvFab1.getLayoutParams();
+            layoutParams.rightMargin -= (int) (tvFab1.getWidth() * 3.8);
+            layoutParams.bottomMargin -= (int) (tvFab1.getHeight() * 0);
+            tvFab1.setLayoutParams(layoutParams);
+            tvFab1.startAnimation(hide_tvfab_1);
+            tvFab1.setClickable(false);
+
             layoutParams = (FrameLayout.LayoutParams) fab2.getLayoutParams();
             layoutParams.rightMargin -= (int) (fab2.getWidth() * 1.1);
             layoutParams.bottomMargin -= (int) (fab2.getHeight() * 1.1);
             fab2.setLayoutParams(layoutParams);
             fab2.startAnimation(hide_fab_2);
             fab2.setClickable(false);
+            layoutParams = (FrameLayout.LayoutParams) tvFab2.getLayoutParams();
+            layoutParams.rightMargin -= (int) (tvFab2.getWidth() * 2.4);
+            layoutParams.bottomMargin -= (int) (tvFab2.getHeight() * 4.3);
+            tvFab2.setLayoutParams(layoutParams);
+            tvFab2.startAnimation(hide_tvfab_2);
+            tvFab2.setClickable(false);
+
             layoutParams = (FrameLayout.LayoutParams) fab3.getLayoutParams();
             layoutParams.rightMargin -= (int) (fab3.getWidth() * 0);
             layoutParams.bottomMargin -= (int) (fab3.getHeight() * 1.3);
             fab3.setLayoutParams(layoutParams);
             fab3.startAnimation(hide_fab_3);
             fab3.setClickable(false);
+            layoutParams = (FrameLayout.LayoutParams) tvFab3.getLayoutParams();
+            layoutParams.rightMargin -= (int) (tvFab3.getWidth() * 0);
+            layoutParams.bottomMargin -= (int) (tvFab3.getHeight() * 6.3);
+            tvFab3.setLayoutParams(layoutParams);
+            tvFab3.startAnimation(hide_tvfab_3);
+            tvFab3.setClickable(false);
+
             layoutParams = (FrameLayout.LayoutParams) fab4.getLayoutParams();
             layoutParams.rightMargin += (int) (fab4.getWidth() * 1.1);
             layoutParams.bottomMargin -= (int) (fab4.getHeight() * 1.1);
             fab4.setLayoutParams(layoutParams);
             fab4.startAnimation(hide_fab_4);
             fab4.setClickable(true);
+            layoutParams = (FrameLayout.LayoutParams) tvFab4.getLayoutParams();
+            layoutParams.rightMargin += (int) (tvFab4.getWidth() * 2.5);
+            layoutParams.bottomMargin -= (int) (tvFab4.getHeight() * 4.6);
+            tvFab4.setLayoutParams(layoutParams);
+            tvFab4.startAnimation(hide_fab_4);
+            tvFab4.setClickable(true);
+
             layoutParams = (FrameLayout.LayoutParams) fab5.getLayoutParams();
             layoutParams.rightMargin += (int) (fab5.getWidth() * 1.3);
             layoutParams.bottomMargin += (int) (fab5.getHeight() * 0);
             fab5.setLayoutParams(layoutParams);
             fab5.startAnimation(hide_fab_5);
             fab5.setClickable(true);
+            layoutParams = (FrameLayout.LayoutParams) tvFab5.getLayoutParams();
+            layoutParams.rightMargin += (int) (tvFab5.getWidth() * 5.0);
+            layoutParams.bottomMargin += (int) (tvFab5.getHeight() * 0);
+            tvFab5.setLayoutParams(layoutParams);
+            tvFab5.startAnimation(hide_tvfab_5);
+            tvFab5.setClickable(true);
+
             layoutParams = (FrameLayout.LayoutParams) fab6.getLayoutParams();
             layoutParams.rightMargin += (int) (fab6.getWidth() * 1.1);
             layoutParams.bottomMargin += (int) (fab6.getHeight() * 1.1);
             fab6.setLayoutParams(layoutParams);
             fab6.startAnimation(hide_fab_6);
             fab6.setClickable(true);
+            layoutParams = (FrameLayout.LayoutParams) tvFab6.getLayoutParams();
+            layoutParams.rightMargin += (int) (tvFab6.getWidth() * 1.3);
+            layoutParams.bottomMargin += (int) (tvFab6.getHeight() * 5.1);
+            tvFab6.setLayoutParams(layoutParams);
+            tvFab6.startAnimation(hide_tvfab_6);
+            tvFab6.setClickable(true);
+
             layoutParams = (FrameLayout.LayoutParams) fab7.getLayoutParams();
             layoutParams.rightMargin += (int) (fab7.getWidth() * 0);
             layoutParams.bottomMargin += (int) (fab7.getHeight() * 1.3);
             fab7.setLayoutParams(layoutParams);
             fab7.startAnimation(hide_fab_7);
             fab7.setClickable(true);
+
             layoutParams = (FrameLayout.LayoutParams) fab8.getLayoutParams();
             layoutParams.rightMargin -= (int) (fab8.getWidth() * 1.1);
             layoutParams.bottomMargin += (int) (fab8.getHeight() * 1.1);
             fab8.setLayoutParams(layoutParams);
             fab8.startAnimation(hide_fab_8);
             fab8.setClickable(true);
+            layoutParams = (FrameLayout.LayoutParams) tvFab8.getLayoutParams();
+            layoutParams.rightMargin -= (int) (tvFab8.getWidth() * 1.3);
+            layoutParams.bottomMargin += (int) (tvFab8.getHeight() * 5.1);
+            tvFab8.setLayoutParams(layoutParams);
+            tvFab8.startAnimation(hide_tvfab_8);
+            tvFab8.setClickable(true);
 
             //TODO: colocar un boton en el menú que muestre la Toolbar.5
         }
