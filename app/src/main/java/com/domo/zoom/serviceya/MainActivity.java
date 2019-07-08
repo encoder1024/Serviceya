bc4b28505258f2bea237ae26e640c401e29d5b41
encoder1024 <48453885+encoder1024@users.noreply.github.com>
2019-07-08 18:10:53 -0300

    Para emitir release1.5 a Producción

--- app/src/main/java/com/domo/zoom/serviceya/MainActivity.java
+++ app/src/main/java/com/domo/zoom/serviceya/MainActivity.java
@@ -44,7 +44,7 @@ import android.widget.Toast;
 //Comento para alfa-release1.4-out
 //Comento para alfa-release1.4
 //Comento para alfa-release1.5-ini
-//Comento para alfa-release1.5-out
+//Comento para alfa-release1.5-out(Se trasnformó el App de Producción)
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
